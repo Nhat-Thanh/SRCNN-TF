44262f1a3c714f57dc35627f9a2160fd546827e2
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 09:59:29 +0700

    add main functions and fix logic errors

--- demo.py
+++ demo.py
@@ -6,51 +6,59 @@ from model import SRCNN
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--scale',        type=float, default=2,                                  help='-')
-parser.add_argument('--architecture', type=str,   default="915",                              help='-')
-parser.add_argument("--image-path",   type=str,   default="dataset/test1.png",                help='-')
-parser.add_argument("--ckpt-path",    type=str,   default="checkpoint/SRCNN915/SRCNN-915.h5", help='-')
+parser.add_argument('--scale',        type=int, default=2,                   help='-')
+parser.add_argument('--architecture', type=str, default="915",               help='-')
+parser.add_argument("--ckpt-path",    type=str, default="",                  help='-')
+parser.add_argument("--image-path",   type=str, default="dataset/test1.png", help='-')
+
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAGS, unparsed = parser.parse_known_args()
-architecture = FLAGS.architecture
 image_path = FLAGS.image_path
-ckpt_path = FLAGS.ckpt_path
+
+architecture = FLAGS.architecture
+if architecture not in ["915", "935", "955"]:
+    raise ValueError("architecture must be 915, 935 or 955")
+
 scale = FLAGS.scale
+if scale not in [2, 3, 4]:
+    raise ValueError("scale must be 2, 3, or 4")
 
-if scale < 1 or scale > 5:
-    ValueError("scale should be positive and less than 5")
+ckpt_path = FLAGS.ckpt_path
+if (ckpt_path == "") or (ckpt_path == "default"):
+    ckpt_path = f"checkpoint/SRCNN{architecture}/SRCNN-{architecture}.h5"
 
-
-# -----------------------------------------------------------
-#  read image and save bicubic image
-# -----------------------------------------------------------
-
-lr_image = read_image(image_path)
-bicubic_image = upscale(lr_image, scale)
-write_image("bicubic.png", bicubic_image)
+sigma = 0.3 if scale == 2 else 0.2
+pad = int(architecture[1]) // 2 + 6
 
 
 # -----------------------------------------------------------
-# preprocess lr image 
+# demo
 # -----------------------------------------------------------
 
-lr_image = gaussian_blur(lr_image, sigma=0.3)
-lr_image = upscale(lr_image, scale)
-lr_image = rgb2ycbcr(lr_image)
-lr_image = norm01(lr_image)
-lr_image = tf.expand_dims(lr_image, axis=0)
+def main():
+    lr_image = read_image(image_path)
+    bicubic_image = upscale(lr_image, scale)
+    bicubic_image = bicubic_image[pad:-pad, pad:-pad]
+    write_image("bicubic.png", bicubic_image)
 
+    lr_image = gaussian_blur(lr_image, sigma=sigma)
+    bicubic_image = upscale(lr_image, scale)
+    bicubic_image = rgb2ycbcr(bicubic_image)
+    bicubic_image = norm01(bicubic_image)
+    bicubic_image = tf.expand_dims(bicubic_image, axis=0)
 
-# -----------------------------------------------------------
-#  predict and save image
-# -----------------------------------------------------------
+    model = SRCNN(architecture)
+    model.load_weights(ckpt_path)
+    sr_image = model.predict(bicubic_image)[0]
 
-model = SRCNN(architecture)
-model.load_weights(ckpt_path)
-sr_image = model.predict(lr_image)[0]
+    sr_image = denorm01(sr_image)
+    sr_image = tf.cast(sr_image, tf.uint8)
+    sr_image = ycbcr2rgb(sr_image)
+    write_image("sr.png", sr_image)
 
-sr_image = denorm01(sr_image)
-sr_image = tf.cast(sr_image, tf.uint8)
-sr_image = ycbcr2rgb(sr_image)
-
-write_image("sr.png", sr_image)
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -6,56 +6,64 @@ from model import SRCNN
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--scale',          type=int, default=2,     help='-')
-parser.add_argument("--architecture",   type=str, default="915", help='-')
+parser.add_argument('--scale',        type=int, default=2,     help='-')
+parser.add_argument('--architecture', type=str, default="915", help='-')
+parser.add_argument("--ckpt-path",    type=str, default="",    help='-')
+
+# -----------------------------------------------------------
+# global variables
+# -----------------------------------------------------------
 
 FLAGS, unparsed = parser.parse_known_args()
-scale = FLAGS.scale
+
 architecture = FLAGS.architecture
-k = 6 + int(architecture[1]) // 2
-
-if scale not in [2, 3, 4]:
-    ValueError("scale must be 2, 3, or 4")
-
 if architecture not in ["915", "935", "955"]:
-    ValueError("architecture must be 915, 935 or 955")
+    raise ValueError("architecture must be 915, 935 or 955")
+
+scale = FLAGS.scale
+if scale not in [2, 3, 4]:
+    raise ValueError("scale must be 2, 3, or 4")
+
+ckpt_path = FLAGS.ckpt_path
+if (ckpt_path == "") or (ckpt_path == "default"):
+    ckpt_path = f"checkpoint/SRCNN{architecture}/SRCNN-{architecture}.h5"
+
+sigma = 0.3 if scale == 2 else 0.2
+pad = int(architecture[1]) // 2 + 6
+
 
 # -----------------------------------------------------------
-#  load model
+# test
 # -----------------------------------------------------------
 
-ckpt_path = f"checkpoint/SRCNN{architecture}/SRCNN-{architecture}.h5"
+def main():
+    model = SRCNN(architecture)
+    model.load_weights(ckpt_path)
 
-model = SRCNN(architecture)
-model.load_weights(ckpt_path)
+    ls_data = sorted_list(f"dataset/test/x{scale}/data")
+    ls_labels = sorted_list(f"dataset/test/x{scale}/labels")
+
+    sum_psnr = 0
+    for i in range(0, len(ls_data)):
+        lr_image = read_image(ls_data[i])
+        lr_image = gaussian_blur(lr_image, sigma=sigma)
+        bicubic_image = upscale(lr_image, scale)
+        hr_image = read_image(ls_labels[i])
+        hr_image = hr_image[pad:-pad, pad:-pad]
+
+        bicubic_image = rgb2ycbcr(bicubic_image)
+        hr_image = rgb2ycbcr(hr_image)
+
+        bicubic_image = norm01(bicubic_image)
+        hr_image = norm01(hr_image)
+
+        bicubic_image = tf.expand_dims(bicubic_image, axis=0)
+        sr_image = model.predict(bicubic_image)[0]
+
+        sum_psnr += PSNR(hr_image, sr_image, max_val=1).numpy()
+
+    print(sum_psnr / len(ls_data))
 
 
-# -----------------------------------------------------------
-# test 
-# -----------------------------------------------------------
-
-ls_data = sorted_list(f"dataset/test/x{scale}/data")
-ls_labels = sorted_list(f"dataset/test/x{scale}/labels")
-
-sum_psnr = 0
-for i in range(0, len(ls_data)):
-    lr_image = read_image(ls_data[i])
-    hr_image = read_image(ls_labels[i])
-
-    lr_image = gaussian_blur(lr_image, sigma=0.3)
-    lr_image = upscale(lr_image, scale)
-    hr_image = hr_image[k:-k, k:-k]
-
-    lr_image = rgb2ycbcr(lr_image)
-    hr_image = rgb2ycbcr(hr_image)
-
-    lr_image = norm01(lr_image)
-    hr_image = norm01(hr_image)
-
-    lr_image = tf.expand_dims(lr_image, axis=0)
-    sr_image = model.predict(lr_image)[0]
-
-    sum_psnr += PSNR(hr_image, sr_image, max_val=1).numpy()
-
-print(sum_psnr / len(ls_data))
-
+if __name__ == "__main__":
+    main()
